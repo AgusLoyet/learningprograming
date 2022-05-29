01112eba31bc4ef76638d6c6b458db086d486ffe
AgusLoyet <agusloyet@gmail.com>
2022-05-29 19:12:34 -0300

    homework 2...7 add

---
+++ Homework 7/helloWorld.js
@@ -0,0 +1,24 @@
+let firstVar ='helloWorld2';
+let firstNumber = 26;
+let secondString = '22';
+const SECOND_NUMBER = 33;
+let firstSum = firstNumber + SECOND_NUMBER;
+let thirdString = '33';
+let firstConcat = secondString + thirdString;
+let firstBoolean = true;
+let firstNull =  null;
+firstNumber = 44;
+firstNumber = 25+33;;
+firstNumber = 'soy un string';
+
+
+console.log(SECOND_NUMBER)
+console.log(firstNull)
+console.log(firstNumber)
+console.log(firstBoolean)
+console.log(firstConcat)
+console.log(firstSum)
+console.log(secondString)
+console.log('helloWorld');
+console.log(firstVar);
+//alert('helloWorld');
